b63e5f50273dfde696b18be7cf1cfd9b2611d7a9
Simon <nakhonekhammanivong@gmail.com>
2025-05-03 14:20:23 +0700

    fixed code

--- JavaScript NakhoneCode/JavaScript/script2.js
+++ JavaScript NakhoneCode/JavaScript/script2.js
@@ -3,6 +3,7 @@ const op = document.getElementById('output');
 output.textContent = 'Yes!';
 output.style.color = 'green';
 
+
 const idk1 = document.getElementById('idk');
 
 idk1.textContent = 'Ask the person next to you!';
